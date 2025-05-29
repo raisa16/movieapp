9877e4c599f4acf12976de1bfd288c02b9d6da1a
Raisa <git>
2025-05-29 01:26:00 -0700

    add trending and categories of movies

---
+++ src/main.js
@@ -0,0 +1,51 @@
+async function getTrendingMoviesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+  const data = await res.json();
+  
+  const movies = data.results;
+  movies.forEach(movie => {
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.Title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer)
+
+  });
+  console.log({data, movies})
+}
+
+
+async function getCategoriesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+  const data = await res.json();
+  
+  const categories = data.genres;
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    categoryTitle.classList.add('category-title');
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle)
+    previewCategoriesContainer.appendChild(categoryContainer)
+
+  });
+  console.log({data, movies})
+}
+
+getTrendingMoviesPreview()
+getCategoriesPreview()
