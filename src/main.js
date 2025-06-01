571cca096c10353a1be7f650419ad2113e4a4c0c
Raisa <git>
2025-05-31 22:10:52 -0700

    adding intersection observer to implement lazy loading to trendring movies setion

--- src/main.js
+++ src/main.js
@@ -9,8 +9,18 @@ const api = axios.create({
   },
 });
 
-function createMovies(movies, container) {
+//Utils 
+const lazyLoadeer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+    const url = entry.target.getAttribute('data-img');
+    entry.target.setAttribute('src', url)
+  }
+})
+})
+function createMovies(movies, container, lazyLoad = false) {
   container.innerHTML = '';
+
   movies.forEach(movie => {
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -22,9 +32,13 @@ function createMovies(movies, container) {
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.Title);
     movieImg.setAttribute(
-      'src',
+    lazyLoad ? 'data-img' : 'src',
       'https://image.tmdb.org/t/p/w300' + movie.poster_path
     );
+
+    if(lazyLoad) {
+      lazyLoadeer.observe(movieImg);
+    }
 
     movieContainer.appendChild(movieImg);
     container.appendChild(movieContainer)
@@ -55,7 +69,7 @@ async function getTrendingMoviesPreview() {
   const {data} = await api('trending/movie/day');  
   const movies = data.results;
 
-  createMovies(movies, trendingMoviesPreviewList)
+  createMovies(movies, trendingMoviesPreviewList, true)
   console.log({data, movies})
 }
 
